8ca7244ee7a1a95ff0985520d0f9f0511c30d459
whistle-boy <joon90s@gmail.com>
2020-12-02 20:52:19 +0900

    TIL

--- crawler/crawler/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-# class CrawlerItem(scrapy.Item):
-#     title = scrapy.Field()
-#     s_price = scrapy.Field()
-#     o_price = scrapy.Field()
-#     discount_rate = scrapy.Field()
-#     link = scrapy.Field()
